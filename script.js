5990686d292a7aa25c38ab21b6476104d30faca2
Mateusz Widerski <mat.widerski@gmail.com>
2018-03-15 21:02:15 +0100

    Add scripts

---
+++ script.js
@@ -0,0 +1,32 @@
+$(function() {
+
+	function Button(text) {
+		this.text = text || 'Default';
+	}
+
+	Button.prototype = {
+		create: function() {
+			var self = this;
+			this.$element = $('<button>');
+			this.$element.text(this.text);
+
+			this.$element.click(function() {
+				alert(self.text);
+			});
+
+			this.$element.appendTo($('body')); //or $('body').append(this.$element);
+		}
+	}
+
+	var btn1 = new Button('Hello!');
+
+	btn1.create();
+
+	var btn2 = new Button('Click me!');
+
+	btn2.create();
+
+	var btnDefault = new Button();
+
+	btnDefault.create();
+});
